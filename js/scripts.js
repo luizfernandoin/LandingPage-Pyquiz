197af49bf7d92f440fe6d97f0ae9337e01fb737d
Luiz Fernando <luizfernandoinfo100@gmail.com>
2023-01-10 21:39:39 -0300

    commit inicial

---
+++ js/scripts.js
@@ -0,0 +1,73 @@
+/*!
+* Start Bootstrap - Agency v7.0.11 (https://startbootstrap.com/theme/agency)
+* Copyright 2013-2022 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-agency/blob/master/LICENSE)
+*/
+//
+// Scripts
+// 
+const elementos = document.querySelectorAll('[data-anima]');
+const animacaoClass = 'animacao';
+
+function animaScroll() {
+    const topoPaginaNaJanela = window.pageYOffset+((window.innerHeight*3)/4);
+    elementos.forEach(function(elemento){
+        if (topoPaginaNaJanela > elemento.offsetTop){
+            elemento.classList.add(animacaoClass);
+        } else {
+            elemento.classList.remove(animacaoClass)
+        }
+    });
+}
+
+if (elementos.length){
+    window.addEventListener('scroll', function(){
+        animaScroll();
+    })
+}
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Navbar shrink function
+    var navbarShrink = function () {
+        const navbarCollapsible = document.body.querySelector('#mainNav');
+        if (!navbarCollapsible) {
+            return;
+        }
+        if (window.scrollY === 0) {
+            navbarCollapsible.classList.remove('navbar-shrink')
+        } else {
+            navbarCollapsible.classList.add('navbar-shrink')
+        }
+
+    };
+
+    // Shrink the navbar 
+    navbarShrink();
+
+    // Shrink the navbar when page is scrolled
+    document.addEventListener('scroll', navbarShrink);
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            offset: 74,
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
